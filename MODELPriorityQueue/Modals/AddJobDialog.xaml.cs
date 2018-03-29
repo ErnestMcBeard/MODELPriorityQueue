c7097e33d01c80b9d48e23344ffa6d53646ff737
jomanrones <32491852+jomanrones@users.noreply.github.com>
2018-03-29 16:33:34 -0500

    Check in for the Add Job modal window

---
+++ MODELPriorityQueue/Modals/AddJobDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MODELPriorityQueue.Modals
+{
+    public sealed partial class AddJobDialog : ContentDialog
+    {
+        public AddJobDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Close the modal window with no saving
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        /// <summary>
+        /// Save and close the modal window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
--- MODELPriorityQueue/Views/MainPage.xaml.cs
+++ MODELPriorityQueue/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MODELPriorityQueue.Modals;
+using System;
 using Windows.UI.Xaml.Controls;
 
 namespace MODELPriorityQueue.Views
@@ -9,6 +10,14 @@ namespace MODELPriorityQueue.Views
         {
             InitializeComponent();
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private async void AddJobButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Implementation will need to be changed later.
+            var dialog = new AddJobDialog();
+            await dialog.ShowAsync();
+
         }
     }
 }
