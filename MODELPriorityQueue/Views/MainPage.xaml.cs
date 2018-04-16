a58164277e2df974d2d7b3b663fd334b6b07080f
Thomas Johnson <tjohnson061944@gmail.com>
2018-04-15 22:12:15 -0500

    Working on queue priority

--- MODELPriorityQueue/Views/MainPage.xaml.cs
+++ MODELPriorityQueue/Views/MainPage.xaml.cs
@@ -59,7 +59,7 @@ namespace MODELPriorityQueue.Views
 
         private void JobsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+            //Nothing
         }
 
         void JobsList_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
@@ -106,6 +106,11 @@ namespace MODELPriorityQueue.Views
         private async void AssignTechnician_Click(object sender, RoutedEventArgs e)
         {
             await ViewModel.AssignTechnician();
+        }
+
+        private async void Prioritize()
+        {
+            await ViewModel.PrioritizeQueue();
         }
     }
 }
