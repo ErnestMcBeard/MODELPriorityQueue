eae968ed76034c57561ff2d21c2a8b328f24b0cb
FedoraLord <jerkrouse@gmail.com>
2018-04-15 22:31:15 -0500

    DailyStatistic stuff

---
+++ MODELPriorityQueue/Models/DailyStatistic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELPriorityQueue.Models
+{
+    public class DailyStatistic : DatabaseEntity<DailyStatistic>
+    {
+        private DateTime date;
+        private int lastQueueLength;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { Set(() => Date, ref date, value); }
+        }
+
+        public int LastQueueLength
+        {
+            get { return lastQueueLength; }
+            set { Set(() => LastQueueLength, ref lastQueueLength, value); }
+        }
+    }
+}
